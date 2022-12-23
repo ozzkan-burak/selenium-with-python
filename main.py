fc849375298ac3dce070e8170bc9b7589323db25
ozzkan-burak <brk.ozzkan@gmail.com>
2022-12-24 00:29:57 +0300

    add input entrie

--- main.py
+++ main.py
@@ -11,3 +11,35 @@ from selenium.webdriver.chrome.service import Service
 service = Service("./webdriver/chromedriver")
 driver = webdriver.Chrome(service=service)
 driver.get("https://www.flo.com.tr")
+driver.maximize_window()
+
+# tarayıcı geri butonu
+#driver.back()
+# tarayıcı lieri butonu
+#driver.forward()
+# tarayıcı refresh butonu
+#driver.refresh()
+
+link = driver.current_url
+title = driver.title
+
+if "flo.com.tr" in link:
+    print("Flo sayfasındayız " + link)
+else:
+    driver.save_screenshot('./screenshot.png')
+print("site link : " + link)
+print('site başlığı : ' + title)
+
+if 'Flo' in title:
+    print('Title doğru')
+else:
+    driver.screenshot('./screenshot1.png')
+
+
+# tarayıcıyı kapatır
+driver.close()
+
+#driver.quit() / selenium un kullandığı tüm pencereleri kapatır
+
+## --- inputlara giriş
+
